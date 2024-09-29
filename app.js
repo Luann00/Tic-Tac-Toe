ba4c78e2c6b0cdbcc7aa0e7e05a12aede875c45f
Luan Zekiri <luan.00@hotmail.de>
2024-09-30 00:03:14 +0200

    implemented game logic in frontend

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+let button1 = document.querySelector("#btn1");
+let button2 = document.querySelector("#btn2");
+let button3 = document.querySelector("#btn3");
+let button4 = document.querySelector("#btn4");
+let button5 = document.querySelector("#btn5");
+let button6 = document.querySelector("#btn6");
+let button7 = document.querySelector("#btn7");
+let button8 = document.querySelector("#btn8");
+let button9 = document.querySelector("#btn9");
+
+
+
+
+
+
+
+
 let gameBoard = (function () {
     let board = ["", "", "", "", "", "", "", "", ""]
 
@@ -5,11 +22,9 @@ let gameBoard = (function () {
 
 
     //Method for overwriting the field
-    const setField = (index, symbol) => {
-        if (index >= 1 && index <= board.length && board[index - 1] == "") {
-            board[index - 1] = symbol;
-            setSignUI(index, sign)
-        }
+    const setField = (symbol, index) => {
+        board[index - 1] = symbol;
+
     };
 
 
@@ -19,6 +34,7 @@ let gameBoard = (function () {
     return { getBoard, setField, getField }
 
 })();
+
 
 
 
@@ -34,7 +50,6 @@ function drawBoard() {
 };
 
 
-
 createPlayer = (name, sign) => {
     let won = false;
 
@@ -43,13 +58,16 @@ createPlayer = (name, sign) => {
     let getName = () => name;
     let getSign = () => sign;
 
-    let startRound = () => playRound(sign)
+    let startRound = () => startNewRound(sign)
     return { getName, getSign, playRound, getWon, setWon, startRound }
 }
 
-
 let player1 = createPlayer("Luan", "X");
 let player2 = createPlayer("Bot", "O");
+let currentPlayer = player1
+
+
+
 
 
 function checkEnd() {
@@ -62,82 +80,61 @@ function checkEnd() {
     return true;
 }
 
-function startNewRound(sign) {
+button1.addEventListener('click', () => {
+    button1.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 1)
+});
 
-}
+button2.addEventListener('click', () => {
+    button2.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 2)
 
+});
+button3.addEventListener('click', () => {
+    button3.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 3)
 
-function playRound(sign) {
+});
+button4.addEventListener('click', () => {
+    button4.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 4)
 
-    let button1 = document.querySelector("#btn1");
-    let button2 = document.querySelector("#btn2");
-    let button3 = document.querySelector("#btn3");
-    let button4 = document.querySelector("#btn4");
-    let button5 = document.querySelector("#btn5");
-    let button6 = document.querySelector("#btn6");
-    let button7 = document.querySelector("#btn7");
-    let button8 = document.querySelector("#btn8");
-    let button9 = document.querySelector("#btn9");
+});
+button5.addEventListener('click', () => {
+    button5.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 5)
 
-    let index = 0;
+});
+button6.addEventListener('click', () => {
+    button6.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 6)
 
+});
+button7.addEventListener('click', () => {
+    button7.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 7)
 
-    button1.addEventListener('click', () => {
-        button1.innerHTML = sign;
-        index = 1;
-    });
+});
+button8.addEventListener('click', () => {
+    button8.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 8)
 
-    button2.addEventListener('click', () => {
-        button2.innerHTML = sign;
-        index = 2;
+});
+button9.addEventListener('click', () => {
+    button9.innerHTML = currentPlayer.getSign();
+    playRound(currentPlayer.getSign(), 9)
 
-    });
-    button3.addEventListener('click', () => {
-        button3.innerHTML = sign;
-        index = 3;
-
-    });
-    button4.addEventListener('click', () => {
-        button4.innerHTML = sign;
-        index = 4;
-
-    });
-    button5.addEventListener('click', () => {
-        button5.innerHTML = sign;
-        index = 5;
-
-    });
-    button6.addEventListener('click', () => {
-        button6.innerHTML = sign;
-        index = 6;
-
-    });
-    button7.addEventListener('click', () => {
-        button7.innerHTML = sign;
-        index = 7;
-
-    });
-    button8.addEventListener('click', () => {
-        button8.innerHTML = sign;
-        index = 8;
-
-    });
-    button9.addEventListener('click', () => {
-        button1.innerHTML = sign;
-        index = 9;
-
-    });
+});
 
 
+function playRound(sign, index) {
 
-
-    gameBoard.setField(index, sign);
-
+    gameBoard.setField(currentPlayer.getSign(), index);
 
     let diagonalNumbers = [1, 5, 9, 7, 5, 3];
 
     if (diagonalNumbers.includes(index)) {
-        if (checkDiagonal(index, sign)) {
+        if (checkDiagonal(index, currentPlayer.getSign())) {
 
         }
     }
@@ -147,17 +144,18 @@ function playRound(sign) {
     if (index == 1 || index == 2 || index == 3) {
         //check for vertical win
         if (gameBoard.getBoard()[0] == gameBoard.getBoard()[1] && gameBoard.getBoard()[0] == gameBoard.getBoard()[2]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
+            announceWinner()
             return;
         } else if (gameBoard.getBoard()[index - 1] == gameBoard.getBoard()[index + 2] && gameBoard.getBoard()[index - 1] == gameBoard.getBoard()[index + 5]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
             return;
         }
@@ -166,17 +164,18 @@ function playRound(sign) {
     if (index == 4 || index == 5 || index == 6) {
         //check for vertical win
         if (gameBoard.getBoard()[3] == gameBoard.getBoard()[4] && gameBoard.getBoard()[3] == gameBoard.getBoard()[5]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
+            announceWinner();
             return;
         } else if (gameBoard.getBoard()[index - 1] == gameBoard.getBoard()[index + 2] && gameBoard.getBoard()[index - 1] == gameBoard.getBoard()[index - 4]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
             return;
         }
@@ -187,21 +186,30 @@ function playRound(sign) {
     if (index == 7 || index == 8 || index == 9) {
         //check for vertical win
         if (gameBoard.getBoard()[6] == gameBoard.getBoard()[7] && gameBoard.getBoard()[6] == gameBoard.getBoard()[8]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
+            announceWinner()
             return;
         } else if (gameBoard.getBoard()[index - 1] == gameBoard.getBoard()[index - 4] && gameBoard.getBoard()[index - 1] == gameBoard.getBoard()[index - 7]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
             return;
         }
 
+    }
+
+
+
+    if (currentPlayer == player2) {
+        currentPlayer = player1
+    } else {
+        currentPlayer = player2
     }
 
     drawBoard();
@@ -214,21 +222,24 @@ function checkDiagonal(index, sign) {
 
     if (index == 1 || index == 5 || index == 9) {
         if (gameBoard.getBoard()[0] == gameBoard.getBoard()[4] && gameBoard.getBoard()[0] == gameBoard.getBoard()[8]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
+            announceWinner()
             return true;
         }
     }
     if (index == 7 || index == 5 || index == 3) {
         if (gameBoard.getBoard()[6] == gameBoard.getBoard()[4] && gameBoard.getBoard()[6] == gameBoard.getBoard()[2]) {
-            if (sign == "X") {
-                player1.setWon();
+            if (currentPlayer.getSign() == "X") {
+                currentPlayer.setWon();
             } else {
-                player2.setWon();
+                currentPlayer.setWon();
             }
+
+            announceWinner()
             return true;
         }
     }
@@ -238,31 +249,37 @@ function checkDiagonal(index, sign) {
 }
 
 
-function playGame() {
-    drawBoard();
-    while (!player1.getWon() && !player2.getWon() && !checkEnd()) {
-        player1.startRound(player1.getSign());
-        if (checkEnd() || player1.getWon() || player2.getWon()) {
-            break;
-        }
-        player2.startRound(player2.getSign());
-    }
-    drawBoard();
 
+function announceWinner() {
 
     if (player1.getWon()) {
         console.log(`${player1.getName()} won the game!`)
     } else if (player2.getWon()) {
         console.log(`${player2.getName()} won the game!`)
-    } else {
+    } else if (checkEnd()) {
         console.log("Tie!")
     }
+
+    disableAllButtons();
 
 }
 
 
+function disableAllButtons() {
+    button1.disabled = true;
+    button2.disabled = true;
+    button3.disabled = true;
+    button4.disabled = true;
+    button5.disabled = true;
+    button6.disabled = true;
+    button7.disabled = true;
+    button8.disabled = true;
+    button9.disabled = true;
 
 
-playGame();
+    document.querySelector("#winnerText").innerHTML = `${currentPlayer.getName()} won the game!`;
+
+}
+
 
 
